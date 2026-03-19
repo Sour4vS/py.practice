43c3b674fd22dd8c05374fa35d0301cebe019ab3
Sour4vS <sour4vS@gmail.com>
2026-03-19 19:59:20 +0530

    Add number guessing game functionality
    Implement number guessing game with input validation.

---
+++ NumberGuess.py
@@ -0,0 +1,33 @@
+import random
+
+num = input("enter a number :")
+if num.isdigit():
+    num = int(num)
+    if num<=0:
+        print("print number greater than 0..")
+        quit()
+else:
+    print("enter a number please :")
+    quit()
+random_number =  random.randint(0,num)
+
+
+while True:
+    guess = input("Guess the number :")
+    if guess.isdigit():
+        guess = int(guess)
+    else:
+        print("please enter a number next time :")
+        continue
+
+    if guess == random_number:
+        print("correct")
+
+        break
+    else:
+        if guess>random_number:
+            print("you guessed above the number")
+        else:
+            print("you guessed below the number ")
+
+print(f"random no is:{random_number}")
