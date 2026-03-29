b7790f8e22ff0a778b540bb2fc61fc7793349be3
Sour4vS <sour4vS@gmail.com>
2026-03-29 11:54:19 +0530

    Implement basic Pig game with player scoring

---
+++ pig_game.py
@@ -0,0 +1,42 @@
+import random
+
+def roll():
+    min =1
+    max =6
+    roll = random.randint(min,max)
+    return roll
+
+while True:
+    players = input("enter number of players from 2 to 4 :")
+    if players.isdigit():
+        players = int(players)
+        if 2<= players <=4:
+            break
+        else:
+            print("enter numbers between 2 and 4..")
+    else:
+        print("enter valid amount..")
+
+
+max_score = 50
+player_score =[0 for _ in range(players)]
+
+while max(player_score) < max_score:
+    for player_indx in range(players):
+        print("Player number", player_indx + 1, "turn has just started! \n")
+        current_score =0
+        while True:
+            should_role = input("do you need to roll?(y)")
+            if should_role.lower() !="y":
+                break
+            value =roll()
+            if value ==1:
+                print("You rolled a 1! Turn done!")
+                current_score =0
+                break
+            else:
+                current_score +=value
+                print("you rolled a: ",value)
+            print("your current score is :",current_score)
+        player_score[player_indx] += current_score
+        print("Your total score is:", player_score[player_indx])
